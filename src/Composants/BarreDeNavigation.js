b32cb5d32d99e15e7d0991c2fda825c024bdd53c
Samuel Renaud <2392263@csfoy.ca>
2024-06-04 14:15:10 -0400

    Ajout du composant BarreDeNavigation

---
+++ src/Composants/BarreDeNavigation.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { RiContactsLine } from 'react-icons/ri';
+
+import Langages from '../i18n/Langages.js';
+
+function BarreDeNavigation({ estAuthentifie }) {
+    const { t } = useTranslation();
+
+    let liens = (
+        <Nav>
+            <Nav.Link 
+                href='/clients'
+                active={window.location.pathname === '/clients'}
+            >
+                {t('clients')}
+            </Nav.Link>
+
+            <Nav.Link
+                href='/creationClient'
+                active={window.location.pathname === '/creationClient'}
+            >
+                {t('creerClient')}
+            </Nav.Link>
+        </Nav>
+    );
+
+    return (
+        <Navbar className='bg-body-tertiary'>
+                <Navbar.Brand href='/' className='ms-5'>
+                    <RiContactsLine 
+                        size={30}
+                        className='d-inline-block align-top'
+                    />
+                    {' ' + t('titreApp')}
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                { estAuthentifie && liens}
+                <Navbar.Collapse className='me-4 justify-content-end'>
+                    <Langages />
+                </Navbar.Collapse>
+        </Navbar>
+    )
+}
+
+export default BarreDeNavigation;
